8e29f9c215eebe49b021a51d877ca36cfae3b23d
strider99 <panigrahiomprakash@gmail.com>
2017-08-01 19:53:17 +0530

    copy multiple items from clipboard and store it.

--- src/main.js
+++ src/main.js
@@ -1,15 +1,46 @@
 const electron = require('electron');
 const path = require('path');
 
-const {app, Tray} = electron;
+const {app, clipboard, Menu, Tray} = electron;
+const STACK_SIZE = 10;
 
 function startApp(){
+	let stack = [];
 	const tray = new Tray(path.join('src','icon.png'));
+	let template = [
+		{
+			label: '<empty>',
+			enabled: false
+		}
+	];
+	let menu = Menu.buildFromTemplate(template);
+	tray.setContextMenu(menu);
+
+	isClipboardChanged(clipboard, text => {
+		stack = addToStack(text, stack);
+		console.log("stack is ",stack);
+	});
+}
+
+function addToStack(item, stack){
+	return [item].concat(stack.length >= STACK_SIZE ? stack.slice(0, stack.length - 1) : stack);
+}
+
+
+
+function isClipboardChanged(clipboard, onChange) {
+	let cache = clipboard.readText();
+	setInterval(() => {
+		latest = clipboard.readText();
+		if(latest !== cache){
+			cache = latest;
+			onChange(cache);
+		}
+	});
 }
 
 function closeApp(){
 	app.quit();
 }
-
 app.on('ready', startApp);
 app.on('closed', closeApp);
